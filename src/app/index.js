9f2e8e2ae6085c4eb656f829e6d40793f4f0d667
devori.lee <devori.lee@gmail.com>
2017-06-04 19:27:53 +0900

    fix bug method name

--- src/app/index.js
+++ src/app/index.js
@@ -31,7 +31,7 @@ setInterval(() => {
 }, 5 * 60 * 1000);
 
 setInterval(() => {
-  autoTraderForBithumb.start('test').catch(reason => {
+  autoTraderForBithumb.trade('test').catch(reason => {
     console.log('[Trader Error] for Bithumb:', reason);
   });
 }, 5 * 60 * 1000 + 3000);
