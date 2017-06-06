70b30fc43789dd134994fdd3ce1afb0806bcae12
devori.lee <devori.lee@gmail.com>
2017-06-07 01:09:04 +0900

    apply USDT per BTC price to calculate alter coin price

--- src/app/collector/poloniex.js
+++ src/app/collector/poloniex.js
@@ -4,32 +4,39 @@ import logger from '../util/logger';
 
 function collect() {
   let priceDB = priceFileDB.load('poloniex');
-  let btcPrice = priceDB.getLast('BTC').price;
+
+  function addToDB(vcType, price) {
+    let data = {
+      price: price,
+      units: 1,
+      timestamp: new Date().getTime()
+    };
+    priceDB.add(vcType, data);
+    let beforeOneDay = new Date(new Date().getTime() - 1000 * 60 * 60 * 24 * 2);
+    priceDB.remove(vcType, info => {
+      return info.timestamp < beforeOneDay.getTime();
+    });
+  }
+
   return new Promise((resolve, reject) => {
     request('https://poloniex.com/public?command=returnTicker', (err, res, body) => {
       let data = JSON.parse(body);
+      let usdtPerBtc = Number(data.USDT_BTC.lowestAsk);
+      let addedData = [];
+      addedData.push(addToDB('BTC', usdtPerBtc));
+
       for (let k in data) {
         let pair = k.split('_');
-        let price = Number(data[k].lowestAsk);
-        if (pair[0] === 'USDT' && pair[1] === 'BTC') {
-          btcPrice = price;
-        } else if (pair[0] === 'BTC') {
-          price =  Math.trunc(price * btcPrice * 100000000) / 100000000;
-        } else {
+        if (pair[0] !== 'BTC') {
           continue;
         }
-        priceDB.add(pair[1], {
-          price: price,
-          units: 1,
-          timestamp: new Date().getTime()
-        });
-        let beforeOneDay = new Date(new Date().getTime() - 1000 * 60 * 60 * 24 * 2);
-        priceDB.remove(pair[1], info => {
-          return info.timestamp < beforeOneDay.getTime();
-        });
+        let price = Number(data[k].lowestAsk);
+        price =  Math.trunc(price * usdtPerBtc * 100000000) / 100000000;
+
+        addedData.push(addToDB(pair[1], price));
       }
       logger.verbose('[Collector-Poloniex] Collected');
-      resolve(data);
+      resolve(addedData);
     });
   });
 }
