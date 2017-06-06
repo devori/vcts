2d53ee5657742f06e52c3b5ce0a0e0cc246dde6e
devori.lee <devori.lee@gmail.com>
2017-06-06 16:20:21 +0900

    remove router of account

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import accountRouter from './account/router';
 import tradeRouter from './trade/router';
 import collectorForBithumb from './collector/bithumb';
 import autoTraderForBithumb from './auto-trader/bithumb';
@@ -9,7 +8,6 @@ import logger from './util/logger';
 let app = express();
 
 app.use(bodyParser.json());
-app.use('/api/v1/accounts', accountRouter);
 app.use('/api/v1/trade', tradeRouter);
 app.use((err, req, res, next) => {
   if (err) {
