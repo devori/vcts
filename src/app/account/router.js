013c0185b92b99600781502efbff152e390a6021
devori.lee <devori.lee@gmail.com>
2017-06-13 19:02:01 +0900

    change api url for history fo trade

--- src/app/account/router.js
+++ src/app/account/router.js
@@ -7,7 +7,7 @@ router.use((req, res, next) => {
   next();
 });
 
-router.get('/:accountId/:vcType?', (req, res) => {
+router.get('/:accountId/history/:vcType?', (req, res) => {
   let history = account.searchHistory(req.params.accountId, req.params.vcType);
   res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
   res.json(history || []);
