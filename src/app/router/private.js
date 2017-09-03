6d8f9c3df5f79cb0694d88fe0b0a6fc30eb30e87
devori.lee <devori.lee@gmail.com>
2017-09-03 16:39:14 +0900

    remove apikey authentication. change api url to add user id

--- src/app/router/private.js
+++ src/app/router/private.js
@@ -4,84 +4,61 @@ import * as account from '../account';
 import logger from '../util/logger';
 
 let router = Router();
-router.all('*', (req, res, next) => {
-  logger.verbose(`[${Date()}] ${req.url} called`);
-  let nonce = req.headers.nonce;
-  if (!nonce || nonce < (new Date().getTime() - 3000)) {
-    res.sendStatus(401);
-    return;
-  }
-  let signObj = req.body || {};
-  signObj.nonce = nonce;
 
-  let apiKey = req.headers['api-key']
-  let signValue =  req.headers['sign'];
-  let auth = account.authenticate(apiKey, signObj, signValue);
-  if (!auth) {
-    res.sendStatus(401);
-    return;
-  }
-  next();
-});
-
-router.get('/markets/:market/assets/:base?/:vcType?', (req, res) => {
-  let apiKey = req.headers['api-key'];
-  let base = req.params.base;
-  let vcType = req.params.vcType
-  let assets = account.searchAssets(apiKey, req.params.market, base, vcType);
+router.get('/users/:user/markets/:market/assets/:base?/:vcType?', (req, res) => {
+  let { user, market, base, vcType } = req.params;
+  let assets = account.searchAssets(user, market, base, vcType);
   res.json(assets);
 });
 
-router.post('/markets/:market/assets/:base/:vcType', (req, res) => {
-  let apiKey = req.headers['api-key'];
-  let keys = account.getMarketKeys(apiKey, req.params.market);
-  marketApi.load(req.params.market).buy(
+router.post('/users/:user/markets/:market/assets/:base/:vcType', (req, res) => {
+  let { user, market, base, vcType } = req.params;
+  let keys = account.getMarketKeys(user, market);
+  marketApi.load(market).buy(
     keys,
-    req.params.base,
-    req.params.vcType,
+    base,
+    vcType,
     req.body.units,
     req.body.price
   ).then(result => {
     result.trades.forEach(t => {
-      account.addAsset(apiKey, req.params.market, t);
-      account.addHistory(apiKey, req.params.market, t);
+      account.addAsset(user, market, t);
+      account.addHistory(user, market, t);
     });
-    res.json(result);
-    logger.info(`[${Date()}] Purchase - ${req.params.base}_${req.params.vcType} : ${req.body.units} - ${req.body.price}`);
-    logger.info(result.trade);
+    res.status(201).json(result);
+    logger.info(`[${Date()}] Purchase - ${base}_${vcType} : ${req.body.units} - ${req.body.price}`);
+    logger.info(result.trades);
   }).catch(err => {
     res.status(500).send(err);
   });
 });
 
 
-router.delete('/markets/:market/assets/:base/:vcType', (req, res) => {
-  let apiKey = req.headers['api-key'];
-  let keys = account.getMarketKeys(apiKey, req.params.market);
+router.delete('/users/:user/markets/:market/assets/:base/:vcType', (req, res) => {
+  let { user, market, base, vcType } = req.params;
+  let keys = account.getMarketKeys(user, market);
   marketApi.load(req.params.market).sell(
     keys,
-    req.params.base,
-    req.params.vcType,
+    base,
+    vcType,
     req.body.units,
     req.body.price
   ).then(result => {
     result.trades.forEach(t => {
-      account.removeAsset(apiKey, req.params.market, t.base, t.vcType, t.units);
-      account.addHistory(apiKey, req.params.market, t);
+      account.removeAsset(user, market, t.base, t.vcType, t.units);
+      account.addHistory(user, market, t);
     });
     res.json(result);
-    logger.info(`[${Date()}] Sale - ${req.params.base}_${req.params.vcType} : ${req.body.units} - ${req.body.price}`);
+    logger.info(`[${Date()}] Sale - ${base}_${vcType} : ${req.body.units} - ${req.body.price}`);
     logger.info(result.raw);
   }).catch(err => {
     res.status(500).send(err);
   });
 });
 
-router.get('/markets/:market/histories/:base?/:vcType?', (req, res) => {
-  let apiKey = req.headers['api-key'];
-  let base = req.params.base;
-  let vcType = req.params.vcType;
-  let result = account.getHistory(apiKey, req.params.market, base, vcType);
+router.get('/users/:user/markets/:market/histories/:base?/:vcType?', (req, res) => {
+  let { user, market, base, vcType } = req.params;
+  let result = account.getHistory(user, market, base, vcType);
   res.json(result);
 });
 
--- src/test/router/private.js
+++ src/test/router/private.js
@@ -9,197 +9,195 @@ import privateRouter from '../../app/router/private';
 import * as account from '../../app/account';
 
 describe('router/private.js', function () {
+  const TEST_USER = 'test-user';
+  const NON_EXIST_USER = 'non-exist-user';
+  const MARKET = 'poloniex';
   const ACCOUNT_API_KEY = 'test-user';
-  const ACCOUNT_VALID_SIGN = 'account-valid-sign';
-  const ACCOUNT_INVALID_SIGN = 'account-invalid-sign';
   const POLONIEX_API_KEY = 'poloniex-api-key';
   const POLONIEX_SECRET_API = 'poloniex-secret-key';
-  const MARKET = 'poloniex';
 
   let mockAccount;
   let app;
-  beforeEach(() => {
-    mockAccount = sinon.mock(account);
 
-		sinon.stub(account, 'authenticate')
-			.withArgs(ACCOUNT_API_KEY, sinon.match.any, ACCOUNT_VALID_SIGN).returns(true)
-			.withArgs(ACCOUNT_API_KEY, sinon.match.any, ACCOUNT_INVALID_SIGN).returns(false);
-
-    sinon.stub(account, 'getHistory').withArgs(ACCOUNT_API_KEY, MARKET, sinon.match.any, sinon.match.any).returns({
-      "USDT": {
-        "BTC": [
-          {
-            "base": "USDT",
-            "vcType": "BTC",
-            "units": 1,
-            "price": 2500,
-            "total": 2500,
-            "type": "sell",
-            "timestamp": 123
-          }
-        ]
-      }
-    });
-
-    sinon.stub(account, 'searchAssets').withArgs(ACCOUNT_API_KEY, MARKET, sinon.match.any, sinon.match.any).returns({
-      "USDT": {
-        "BTC": [
-          {
-            "base": "USDT",
-            "vcType": "BTC",
-            "units": 0.4,
-            "price": 2500,
-            "timestamp": 123,
-            "uuid": "265dac7d-1aaa-46b0-9f46-6dac2f45f44f"
-          }
-        ]
-      }
-    });
-
-    sinon.stub(marketApi.load('poloniex'), 'buy').withArgs({
-      apiKey: POLONIEX_API_KEY,
-      secretKey: POLONIEX_SECRET_API
-    }).callsFake(() => {
-      return Promise.resolve({
-  			trades: [
-          {
-            units: 0.9975,
-            price: 100,
-            total: 100
-          }
-        ],
-  			timestamp: new Date().getTime(),
-  			raw: {}
-  		});
-    });
-
-    sinon.stub(marketApi.load('poloniex'), 'sell').withArgs({
-      apiKey: POLONIEX_API_KEY,
-      secretKey: POLONIEX_SECRET_API
-    }).callsFake(() => {
-      return Promise.resolve({
-        trades: [
-          {
-            units: 1,
-            price: 10000,
-            total: 9975
-          }
-        ],
-        timestamp: new Date().getTime(),
-        raw: {}
-      });
-    });
-
+  before(() => {
     app = express();
     app.use(bodyParser.json());
     app.use('/', privateRouter);
   });
 
-  it('should return assets when authentication is correct', done => {
-    supertest(app)
-			.get('/markets/poloniex/assets')
-      .set('api-key', ACCOUNT_API_KEY)
-      .set('sign', ACCOUNT_VALID_SIGN)
-      .set('nonce', new Date().getTime())
-      .expect('Content-Type', 'application/json; charset=utf-8')
-      .expect(200)
-      .end((err, res) => {
-        if (err) {
-          expect.fail('', '', 'request failure');
-          return;
+  describe('GET /users/:user/markets/:market/assets/:base?/:vcType?', () => {
+    before(() => {
+      sinon.stub(account, 'searchAssets').withArgs(TEST_USER, MARKET, sinon.match.any, sinon.match.any).returns({
+        "USDT": {
+          "BTC": [
+            {
+              "base": "USDT",
+              "vcType": "BTC",
+              "units": 0.4,
+              "price": 2500,
+              "timestamp": 123,
+              "uuid": "265dac7d-1aaa-46b0-9f46-6dac2f45f44f"
+            }
+          ]
         }
-        expect(res.body.USDT).to.exist;
-        expect(res.body.USDT.BTC.length).to.equal(1);
-        expect(res.body.USDT.BTC[0].base).to.equal('USDT');
-        done();
       });
-    this.timeout(3000);
+    });
+    after(() => {
+      account.searchAssets.restore();
+    });
+    it('should return assets when the user exists', done => {
+      supertest(app)
+  			.get(`/users/${TEST_USER}/markets/${MARKET}/assets`)
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(200)
+        .end((err, res) => {
+          if (err) {
+            expect.fail('', '', err);
+            return;
+          }
+          expect(res.body.USDT).to.exist;
+          expect(res.body.USDT.BTC.length).to.equal(1);
+          expect(res.body.USDT.BTC[0].base).to.equal('USDT');
+          done();
+        });
+      this.timeout(3000);
+    });
   });
 
-  it('should return 401 status code when authentication is incorrect', () => {
-    supertest(app)
-      .get('/')
-      .set('api-key', ACCOUNT_API_KEY)
-      .set('sign', ACCOUNT_INVALID_SIGN)
-      .set('nonce', new Date().getTime())
-      .expect(401)
-  });
-
-  it('should return 401 status code when nonce is less than current timestamp - 3000', () => {
-    supertest(app)
-      .get('/')
-      .set('api-key', ACCOUNT_API_KEY)
-      .set('sign', ACCOUNT_INVALID_SIGN)
-      .set('nonce', 123)
-      .expect(401)
-  });
-
-  it('should return buy result after buy info save when buy call', done => {
-    mockAccount.expects('addAsset').withArgs('test-user', 'poloniex').once();
-    mockAccount.expects('addHistory').withArgs('test-user', 'poloniex').once();
-    supertest(app)
-      .post('/markets/poloniex/assets/USDT/BTC')
-      .send({
-        units: 1,
-        price: 100
-      })
-      .set('api-key', ACCOUNT_API_KEY)
-      .set('sign', ACCOUNT_VALID_SIGN)
-      .set('nonce', new Date().getTime())
-      .expect('Content-Type', 'application/json; charset=utf-8')
-      .expect(200)
-      .end((err, res) => {
-        mockAccount.verify();
-        done();
+  describe('POST /users/:user/markets/:market/assets/:base?/:vcType?', () => {
+    let mockAccount;
+    before(() => {
+      mockAccount = sinon.mock(account);
+      sinon.stub(marketApi.load(MARKET), 'buy').withArgs({
+        apiKey: POLONIEX_API_KEY,
+        secretKey: POLONIEX_SECRET_API
+      }).callsFake(() => {
+        return Promise.resolve({
+    			trades: [
+            {
+              units: 0.9975,
+              price: 100,
+              total: 100
+            }
+          ],
+    			timestamp: new Date().getTime(),
+    			raw: {}
+    		});
       });
-    this.timeout(3000);
+    });
+    after(() => {
+      marketApi.load(MARKET).buy.restore();
+      mockAccount.restore();
+    });
+    it('should return buy result with 201 after buy info save when url called', done => {
+      mockAccount.expects('addAsset').withArgs(TEST_USER, MARKET).once();
+      mockAccount.expects('addHistory').withArgs(TEST_USER, MARKET).once();
+      supertest(app)
+        .post(`/users/${TEST_USER}/markets/${MARKET}/assets/USDT/BTC`)
+        .send({
+          units: 1,
+          price: 100
+        })
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(201)
+        .end((err, res) => {
+          if (err) {
+            expect.fail('', '', err);
+            return;
+          }
+          mockAccount.verify();
+          done();
+        });
+      this.timeout(3000);
+    });
   });
 
-  it('should return sell result after sell info save when sell call', done => {
-    mockAccount.expects('removeAsset').withArgs('test-user', 'poloniex').once();
-    mockAccount.expects('addHistory').withArgs('test-user', 'poloniex').once();
-    supertest(app)
-      .delete('/markets/poloniex/assets/USDT/BTC')
-      .send({
-        units: 1,
-        price: 10000
-      })
-      .set('api-key', ACCOUNT_API_KEY)
-      .set('sign', ACCOUNT_VALID_SIGN)
-      .set('nonce', new Date().getTime())
-      .expect('Content-Type', 'application/json; charset=utf-8')
-      .expect(200)
-      .end((err, res) => {
-        mockAccount.verify();
-        done();
+  describe('DELETE /users/:user/markets/:market/assets/:base?/:vcType?', () => {
+    let mockAccount;
+    before(() => {
+      mockAccount = sinon.mock(account);
+      sinon.stub(marketApi.load(MARKET), 'sell').withArgs({
+        apiKey: POLONIEX_API_KEY,
+        secretKey: POLONIEX_SECRET_API
+      }).callsFake(() => {
+        return Promise.resolve({
+          trades: [
+            {
+              units: 1,
+              price: 10000,
+              total: 9975
+            }
+          ],
+          timestamp: new Date().getTime(),
+          raw: {}
+        });
       });
-    this.timeout(3000);
+    });
+    after(() => {
+      marketApi.load(MARKET).sell.restore();
+      mockAccount.restore();
+    });
+    it('should return sell result after sell info save when sell call', done => {
+      mockAccount.expects('removeAsset').withArgs(TEST_USER, MARKET).once();
+      mockAccount.expects('addHistory').withArgs(TEST_USER, MARKET).once();
+      supertest(app)
+        .delete(`/users/${TEST_USER}/markets/${MARKET}/assets/USDT/BTC`)
+        .send({
+          units: 1,
+          price: 10000
+        })
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(200)
+        .end((err, res) => {
+          if (err) {
+            expect.fail('', '', err);
+            return;
+          }
+          mockAccount.verify();
+          done();
+        });
+      this.timeout(3000);
+    });
   });
 
-  it('should return history matched condition when histories call using get method', done => {
-    supertest(app)
-      .get('/markets/poloniex/histories/USDT/BTC')
-      .set('api-key', ACCOUNT_API_KEY)
-      .set('sign', ACCOUNT_VALID_SIGN)
-      .set('nonce', new Date().getTime())
-      .expect('Content-Type', 'application/json; charset=utf-8')
-      .expect(200)
-      .end((err, res) => {
-        expect(res.body.USDT).to.exist;
-        expect(res.body.USDT.BTC).to.exist;
-        expect(res.body.USDT.BTC.length).to.equal(1);
-        expect(res.body.USDT.BTC[0].base).to.equal('USDT');
-        done();
+  describe('GET /users/:user/markets/:market/histories/:base?/:vcType?', () => {
+    before(() => {
+      sinon.stub(account, 'getHistory').withArgs(ACCOUNT_API_KEY, MARKET, sinon.match.any, sinon.match.any).returns({
+        "USDT": {
+          "BTC": [
+            {
+              "base": "USDT",
+              "vcType": "BTC",
+              "units": 1,
+              "price": 2500,
+              "total": 2500,
+              "type": "sell",
+              "timestamp": 123
+            }
+          ]
+        }
       });
-    this.timeout(3000);
-  });
-
-  afterEach(() => {
-		account.authenticate.restore();
-    account.getHistory.restore();
-    account.searchAssets.restore();
-    mockAccount.restore();
-    marketApi.load('poloniex').buy.restore();
-    marketApi.load('poloniex').sell.restore();
+    });
+    after(() => {
+      account.getHistory.restore();
+    });
+    it('should return history matched condition when histories call using get method', done => {
+      supertest(app)
+        .get(`/users/${TEST_USER}/markets/${MARKET}/histories/USDT/BTC`)
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(200)
+        .end((err, res) => {
+          if (err) {
+            expect.fail('', '', err);
+            return;
+          }
+          expect(res.body.USDT).to.exist;
+          expect(res.body.USDT.BTC).to.exist;
+          expect(res.body.USDT.BTC.length).to.equal(1);
+          expect(res.body.USDT.BTC[0].base).to.equal('USDT');
+          done();
+        });
+      this.timeout(3000);
+    });
   });
 });
